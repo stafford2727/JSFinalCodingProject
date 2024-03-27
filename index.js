e238f4ee68a0310b90858de6908277193767a28c
stafford2727 <157815226+stafford2727@users.noreply.github.com>
2024-03-26 21:51:44 -0600

    final project commit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 
-const VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-const SUITS = ['♠', '♥', '♣', '♦']
+const VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const SUITS = ['♠', '♥', '♣', '♦'];
+const CARDVAULES = { '2' : 2, '3' : 3, '4' : 4, '5' : 5, '6' : 6, '7' : 7, '8' : 8, '9' : 9, '10': 10, 'J' : 11, 'Q' : 12, 'K' : 13, 'A' : 14};
+let player1 
+let player2 
 
 /* fist class to determine card drawn/played and the value*/
-
+ 
 class Deck {
     constructor(allCards = newDeck()) {
         this.allCards = allCards
+
     }
 
         get numberOfCards() {
@@ -36,22 +40,38 @@ class NewCard {
             })
     })
    }
-const deck = new Deck();
 
-class Dealer {
+   /*class DealCards {
     constructor(player1, player2) {
-        this.player1 = [];
-        this.player2 = [];
-    }
-}
-    
+        this.player1 = 
+        this.player2 = 
+    } 
+   }*/
 
-begin()
+beginGame()
     function beginGame() {
-       shuffle()
-       newDeck()
-        for(let i = 0; i < this.player1.length; i++) {
-            push.newDeck(player1[i])
+        const deck = new Deck();
+        deck.shuffle()
+
+        halfDeck = (deck.numberOfCards / 2) // cuts the deck in half
+        player1 = new Deck(deck.allCards.slice(0, halfDeck))
+        player2 = new Deck(deck.allCards.slice(halfDeck, deck.numberOfCards))
+
+        console.log(player1)
+        console.log(player2)
         }
-    }
-    
+
+        if(youGetAPoint(player1, player2)) {
+        let i = 0
+        let j = 0
+            if(player1 > player2 ) {
+                player1 = i + 1
+            }
+           } else {
+            player2 = j + 1
+           }
+        
+           function youGetAPoint(cardOne, cardTwo) {
+            return CARDVAULES[cardOne.value] > CARDVAULES[cardTwo.value]
+           }
+        
